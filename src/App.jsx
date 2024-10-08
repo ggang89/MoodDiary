7ca64251cedbe17f51cbd1b95527c41dd749f7eb
ggang89 <ggang89@gmail.com>
2024-10-09 04:51:45 +0900

    router 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import New from "./pages/New"
+import Diary from "./pages/Diary";
 import "./App.css";
 
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<h1>Not  Found</h1> } />
+    </Routes>
+  );
 }
 
 export default App;
