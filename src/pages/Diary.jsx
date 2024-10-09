65027dc791fa04c4d57df1af8261c22e357404dc
ggang89 <ggang89@gmail.com>
2024-10-09 22:11:15 +0900

    동적경로 설정하는 방법 2가지 1)URL Parameter(useParams이용) 2)Query Strig(useSearchParams이용)

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,3 +1,10 @@
+import { useParams } from "react-router-dom";
+
 export default function Diary() {
-  return <></>;
+  const { id } = useParams();
+  return (
+    <>
+      <h1>{id}번째 다이어리입니다</h1>
+    </>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function Home() {
-  return <></>;
+  const [params, setParams] = useSearchParams();
+  //주소설정: localhost:5173/?value=hello
+  console.log(params.get("value")); //hello 출력
+
+  return <div>home</div>;
 }
