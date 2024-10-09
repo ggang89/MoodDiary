700d6f7c2b1e1a2e898f5b2ee826513b79b4c2be
ggang89 <ggang89@gmail.com>
2024-10-10 03:14:55 +0900

    reducer함수 정의해주고 이벤트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useRef } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -22,11 +22,54 @@ const mockData = [
 ];
 
 function reducer(state, action) {
-  return state;
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item)=>String(item.id) !== String(action.id))
+    default: return state;
+  }
 }
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3); //3번부터 시작하도록
 
+  //새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  //기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+  //기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    })
+  };
   return (
     <>
       <Routes>
