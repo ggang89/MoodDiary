df38a5c6a6390c6db228b4eb0f0c2c4b2189043e
ggang89 <ggang89@gmail.com>
2024-10-10 00:54:26 +0900

    이미지 import문을 따로 폴더로 뺀 후 함수로 불러오도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import "./App.css";
+import { getEmotionImage } from "./util/get-emotion-image";
 
-import emotion1 from "./assets/emotion1.png";
-import emotion2 from "./assets/emotion2.png";
-import emotion3 from "./assets/emotion3.png";
-import emotion4 from "./assets/emotion4.png";
-import emotion5 from "./assets/emotion5.png";
 
 function App() {
   const nav = useNavigate();
@@ -20,11 +16,11 @@ function App() {
   return (
     <>
       <div>
-        <img src={emotion1} />
-        <img src={emotion2} />
-        <img src={emotion3} />
-        <img src={emotion4} />
-        <img src={emotion5} />
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
       </div>
       <div>
         <Link to={"/"}>HOME</Link>
