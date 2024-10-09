519efd8b04db561ae5ca10b63997ed8c388523ad
ggang89 <ggang89@gmail.com>
2024-10-10 01:52:45 +0900

    state연결하고 mockData만들어서 초기값으로 설정해줌 => useReducer 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
+import { useReducer } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
 import "./App.css";
-import { getEmotionImage } from "./util/get-emotion-image";
-import Button from "./components/Button";
-import Header from "./components/Header";
 
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 function App() {
-  const nav = useNavigate();
-
-  const onClickBtn = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-      <Header title={"Header"} leftChild={<Button text={"Left"}/>} rightChild={<Button text={"Right"}/>}/>
-      
-      <Button />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </>
