478e5f090acb4a981cd707b4201023aa744da11e
ggang89 <ggang89@gmail.com>
2024-10-09 21:53:10 +0900

    Link(클릭시 이동),useNavigate(이벤트 발생시 이동) 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
-import New from "./pages/New"
+import New from "./pages/New";
 import Diary from "./pages/Diary";
 import "./App.css";
 
-
 function App() {
+  const nav = useNavigate();
+
+  const onClickBtn = () => {
+    nav("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<h1>Not  Found</h1> } />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>HOME</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickBtn}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </>
   );
 }
 
