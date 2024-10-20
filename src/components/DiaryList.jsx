200f759dfcb8c03758f0da9edd41192ace1df468
ggang89 <ggang89@gmail.com>
2024-10-21 02:02:09 +0900

    날짜별로 해당 일기가 보이도록 필터링해줌

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import "./DiaryList.css";
 import DiaryItem from "./DiaryItem";
 
-export default function DiaryList() {
+export default function DiaryList({ data }) {
   return (
     <div className="DiaryList">
       <div className="menu_bar">
@@ -13,7 +13,9 @@ export default function DiaryList() {
         <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
       </div>
       <div className="list_Wrapper">
-        <DiaryItem />
+        {data.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
