a3d10ddc52ca6621c8edafc217bc405a5a12d9ba
ggang89 <ggang89@gmail.com>
2024-10-21 21:03:40 +0900

    새 일기쓰기 날짜 변경

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
@@ -24,14 +25,55 @@ const emotionList = [
     emotionName: "끔찍함",
   },
 ];
+
+//value의 값은 문자만 인식하기 때문에 new Date()로 얻은 날짜를 문자로 바꿔줘야함
+const getStringedDate = (targetDate) => {
+  //날짜 => yyyy-mm-dd
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${date}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`;
+};
+
 export default function Editor() {
-  const emotionId = 1;
-  
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+      //Mon Oct 21 2024 09:00:00 GMT+0900 (한국 표준시) {}
+    }
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+  const emotionId = 5;
+
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
@@ -50,8 +92,8 @@ export default function Editor() {
         <textarea placeholder="오늘은 어땠나요?" />
       </section>
       <section className="button_section">
-        <Button text={"취소하기"}/>
-        <Button text={"작성완료"} type={"POSITIVE"}/>
+        <Button text={"취소하기"} />
+        <Button text={"작성완료"} type={"POSITIVE"} />
       </section>
     </div>
   );
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,16 +1,17 @@
-import Header from "../components/Header"
-import Button from "../components/Button"
-import Editor from "../components/Editor"
-
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
 
 export default function New() {
-  
+  const nav = useNavigate();
   return (
     <>
-      <Header title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기" } />}/>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+      />
       <Editor />
-      
     </>
-  )
+  );
 }
