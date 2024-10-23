78450acc4bd3520c74a4541d58e421bbca5f9569
ggang89 <ggang89@gmail.com>
2024-10-24 02:11:34 +0900

    편집하기 에서 삭제하기 버튼 기능

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,10 +1,33 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 export default function Edit() {
   const { id } = useParams();
-  
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않습니다!")) {
+      //일기 삭제하기
+      onDelete(id);
+      nav("/", { replace: true });
+    }
+  };
+
   return (
     <>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor />
     </>
-  )
+  );
 }
